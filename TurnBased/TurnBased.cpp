84ef04a39a325c236399c7789a32b285c2059d23
L11X16W13 <L11X16W13@L11WS13>
2023-09-13 17:35:08 +0800

    Fixed Bug. was calling the wrong function

--- TurnBased/TurnBased.cpp
+++ TurnBased/TurnBased.cpp
@@ -4,6 +4,6 @@ int main()
 {
 	PlayerEntity* MainCharacter = new PlayerEntity("Ray", 20, 10, 2, 5, 1, 100);
 	EntityParent* Enemy = new EntityParent("Goblin", 5, 1, 1, 5, 1);
-	MainCharacter->printLevel();
-	Enemy->printLevel();
+	MainCharacter->printStats();
+	Enemy->printStats();
 }
